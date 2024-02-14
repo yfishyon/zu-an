d0bffe3ee14bbaac9a73b627f3b38ac4fcd12ce5
yfishyon <123623739+yfishyon@users.noreply.github.com>
2024-02-14 22:25:19 +0800

    Update book.js

--- book.js
+++ book.js
@@ -1,62 +1,91 @@
-import { segment } from "oicq";
+
+import plugin from '../../../lib/plugins/plugin.js'
 import fetch from "node-fetch";
 
-const _path = process.cwd();
+let list = []
+let k = ""
+let name = ""
+let msg2 = []
+let res = {}
+let data1 = {}
+let data = ""
+let zzss = 0
+let url = ""
+let response = ""
 
-export class books extends plugin {
+export class book extends plugin {
   constructor () {
     super({
-      name: 'books',
+      name: 'book',
       dsc: '搜书',
       event: 'message',
       priority: 500,
       rule: [
         {
-          reg: '^#下载(.*)|#下载(.*)*$',
-          fnc: 'downloadbook'
-        },
-        {
-          reg: '^#搜书(.*)*$',
-          fnc: 'books'
-        },
-      ]
+            reg: "^#搜书(.*)$|#下载(.*)|#取消小说搜索$",
+            fnc: 'book'
+        }
+    ]
     });
   }
 
-  async books(e) {
-    const input = e.msg.replace(/#|＃|搜书| /g, "").trim();
-    if (!input) return e.reply("请输入#搜书 关键词", true);
 
-    let res = await fetch("http://220.167.101.235:5000/list?s=" + input).catch((err) => logger.error(err));
-    if (!res) return e.reply('书籍搜索接口请求失败');
+async book(e) {
+    k = ""
+    if (e.msg.includes("#取消小说搜索") & zzss == 1) {
 
-    const bookData = await res.json();
-    if (!bookData.books || bookData.books.length === 0) {
-        return e.reply('未找到相关书籍');
+        zzss = 0
+        e.reply('已取消当前' + name + '搜索')
     }
-
-    let booklist = bookData.books.map(book => book.replace("list/", ""));
-    
-    for (let i = 0; i < booklist.length; i++) {
-        e.reply(`${i + 1}. ${booklist[i]}`);
+    if (zzss == 1) {
+        e.reply('当前正在搜索中...请勿重复搜索')
+        return
     }
-    
+    if (e.msg.includes("#搜书") & zzss == 0) {
+        zzss = 1
+        msg2 = []
+        list = []
+        k = e.msg.replace(/#搜书/g, "").trim()
+        if (k = ""){
+            res.reply('请输入书籍')
+            return true
+        }
+        name = k
+        e.reply('正在搜索中...请稍后')
+        console.log(k)
+        try {
+            url = 'http://220.167.101.235:5000/list?s=' + k
+            response = await fetch(url);
+            data = await response.json()
+            let res = data.books.map(book => book.replace("list/", ""));
+            console.log(data.books[0].videoId)
+            res = data.books
+
+
+            if (res != undefined) {
+                data1 = {
+                    bookList: res.map((book, index) => `${index + 1}. ${book}`).join('\n')
+                }
+                console.log(data1)
+
+                e.reply(data1)
+                zzss = 0
+
+
+            }
+        } catch (err) {
+            e.reply('未能搜索到 ' + name + '，抱歉')
+            zzss = 0
+            return
+        }
+    }
+    if (e.msg.includes("#下载")) {
+        k = e.msg.replace(/#下载/g, "").trim()
+        console.log(list[Number(k) - 1])
+        let url2 = 'https://Alist.h22.ca/NovelList/' + list[Number(k) - 1]
+        e.reply(url2)
+    }
     return true;
-  }
+}
 
-  async downloadbook(e) {
-    const input = e.msg.replace("#下载", "").trim();
-    if (!input) return e.reply("请输入#下载第n本，例:#下载1", true);
-    const index = parseInt(input) - 1;
-
-    if (isNaN(index) || index < 0 || index >= booklist.length) {
-      return e.reply('指定的书本不存在');
-    }
-
-    const fileName = booklist[index];
-    const url = `https://Alist.h22.ca/NovelList/${fileName}`;
-    e.reply(url);
-
-    return true;
-  }
-      }
+}
